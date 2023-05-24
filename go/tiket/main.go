a8ed71f71b2d036d1541e7c3e9e173221ff2ee59
hildanku <hildankutomo@gmail.com>
2023-05-24 13:02:26 +0700

    Update penyederhaan output

--- go/tiket/main.go
+++ go/tiket/main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 type ticketing struct {
-	NamaPembeli string
-	Jurusan     int
-	JenisBus    int
-	JumlahTiket int
-	TotalBayar  int
+	NamaPembeli  string
+	KodeJurusan  int
+	Jurusan      string
+	KodeJenisBus int
+	JenisBus     string
+	JumlahTiket  int
+	TotalBayar   int
+	Bonus        string
 }
 
 const Diskon float64 = 0.85
@@ -28,99 +31,88 @@ func main() {
 		}
 
 		fmt.Print("Jurusan (1. Bandung 2. Jakarta) : ")
-		fmt.Scanln(&t.Jurusan)
+		fmt.Scanln(&t.KodeJurusan)
 		fmt.Print("Jenis Bus (1. SE 2. AC 3.Non AC ) : ")
-		fmt.Scanln(&t.JenisBus)
+		fmt.Scanln(&t.KodeJenisBus)
 		fmt.Print("Jumlah Tiket : ")
 		fmt.Scanln(&t.JumlahTiket)
-		fmt.Println("=================================================")
 
-		if t.Jurusan == 1 {
-			if t.JenisBus == 1 {
+		if t.KodeJurusan == 1 {
+			t.Jurusan = "Bandung"
+			if t.KodeJenisBus == 1 {
+				t.JenisBus = "Kelas SE"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(BandungSe) * float64(t.JumlahTiket))
+					t.Bonus = "Anda mendapatkan 2x Snack dan 4x Makan"
 				} else {
 					t.TotalBayar = BandungSe * t.JumlahTiket
+					t.Bonus = "Tidak Ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Bandung")
-				fmt.Println("Jenis Bus : Kelas SE")
-				fmt.Println("Bonus : 2x Snack dan 4x Makan")
-				fmt.Println("Jumlah Tiket :", t.JumlahTiket)
-				fmt.Println("Total Bayar : Rp.", t.TotalBayar)
-			} else if t.JenisBus == 2 {
+			} else if t.KodeJenisBus == 2 {
+				t.JenisBus = "Kelas AC"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(BandungAc) * float64(t.JumlahTiket))
+					t.Bonus = "Anda mendapatkan 2x Snack"
 				} else {
 					t.TotalBayar = BandungAc * t.JumlahTiket
+					t.Bonus = "Tidak ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Bandung")
-				fmt.Println("Jenis Bus : Kelas AC")
-				fmt.Println("Bonus : 2x Snack")
-				fmt.Println("Jumlah Tiket:", t.JumlahTiket)
-				fmt.Println("Total Bayar : Rp.", t.TotalBayar)
-
-			} else if t.JenisBus == 3 {
+			} else if t.KodeJenisBus == 3 {
+				t.JenisBus = "Kelas Non AC"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(BandungNonAc) * float64(t.JumlahTiket))
+					t.Bonus = "Tidak ada"
 				} else {
 					t.TotalBayar = BandungNonAc * t.JumlahTiket
+					t.Bonus = "Tidak ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Bandung")
-				fmt.Println("Jenis Bus : Kelas Non AC")
-				fmt.Println("Bonus : Tidak Ada")
-				fmt.Println("Jumlah Tiket:", t.JumlahTiket)
-				fmt.Println("Total Bayar : Rp.", t.TotalBayar)
 			} else {
 				fmt.Println("Anda memilih Kategori yang salah! pilih 1,2 atau 3.")
 			}
 
-		} else if t.Jurusan == 2 {
-			if t.JenisBus == 1 {
+		} else if t.KodeJurusan == 2 {
+			t.Jurusan = "Jakarta"
+			if t.KodeJenisBus == 1 {
+				t.JenisBus = "Kelas SE"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(JakartaSe) * float64(t.JumlahTiket))
+					t.Bonus = "Anda mendapatkan 1x Snack dan 2x Makan"
 				} else {
 					t.TotalBayar = JakartaSe * t.JumlahTiket
+					t.Bonus = "Tidak ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Jakarta")
-				fmt.Println("Jenis Bus : Kelas SE")
-				fmt.Println("Bonus : 2x Snack dan 4x Makan")
-				fmt.Println("Jumlah Tiket:", t.JumlahTiket)
-				fmt.Println("Total Bayar:", t.TotalBayar)
-			} else if t.JenisBus == 2 {
+			} else if t.KodeJenisBus == 2 {
+				t.JenisBus = "Kelas AC"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(JakartaAc) * float64(t.JumlahTiket))
+					t.Bonus = "Anda mendapatkan 1x Snack"
 				} else {
 					t.TotalBayar = JakartaAc * t.JumlahTiket
+					t.Bonus = "Tidak ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Jakarta")
-				fmt.Println("Jenis Bus : Kelas AC")
-				fmt.Println("Bonus : 2x Snack")
-				fmt.Println("Jumlah Tiket:", t.JumlahTiket)
-				fmt.Println("Total Bayar:", t.TotalBayar)
 
-			} else if t.JenisBus == 3 {
+			} else if t.KodeJenisBus == 3 {
+				t.JenisBus = "Kelas Non AC"
 				if t.JumlahTiket > 3 {
 					t.TotalBayar = int(Diskon * float64(JakartaNonAc) * float64(t.JumlahTiket))
+					t.Bonus = "Tidak ada"
 				} else {
 					t.TotalBayar = JakartaNonAc * t.JumlahTiket
+					t.Bonus = "Tidak ada"
 				}
-				fmt.Println("Nama Pembeli:", t.NamaPembeli)
-				fmt.Println("Jurusan : Jakarta")
-				fmt.Println("Jenis Bus : Kelas Non AC")
-				fmt.Println("Bonus : Tidak Ada")
-				fmt.Println("Jumlah Tiket:", t.JumlahTiket)
-				fmt.Println("Total Bayar:", t.TotalBayar)
 			} else {
 				fmt.Println("Anda memilih Kategori Jenis Bus yang salah! pilih 1,2 atau 3.")
 			}
 		} else {
 			fmt.Println("Anda memilih Kategori Jurusan yang salah!. Pilih 1 atau 2")
 		}
+		fmt.Println("=================================================")
+		fmt.Println("Nama Pembeli:", t.NamaPembeli)
+		fmt.Println("Jurusan : ", t.Jurusan)
+		fmt.Println("Jenis Bus : ", t.JenisBus)
+		fmt.Println("Bonus : ", t.Bonus)
+		fmt.Println("Jumlah Tiket:", t.JumlahTiket)
+		fmt.Println("Total Bayar : Rp.", t.TotalBayar)
 		fmt.Println("Terimakasih telah membeli Tiket di Agen P.O Bus JayaJaya-Esport")
 		fmt.Println("=================================================")
 	}
