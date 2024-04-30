3be4cb85a079a00c397707d12c74214deca803e7
Hildan Kusto Utomo <109232248+hildanku@users.noreply.github.com>
2024-04-30 05:31:33 +0000

    chore: documentation

--- KecerdasanBuatan/Tugas2-AI/main.go
+++ KecerdasanBuatan/Tugas2-AI/main.go
@@ -1,22 +1,26 @@
-package main
+package main // inisialisasi nama package
 
 import (
-	"fmt"
+	"fmt" // fmt is a Go package that is used to format basic strings, values, inputs, and outputs. 
 )
 
-func main() {
-	var NilaiAwal, Menu, NilaiAkhir int
+func main() { // buat fungsi main
+	var NilaiAwal, Menu, NilaiAkhir int // menentukan tipe data pada tiap variable
 	// Input nilai awal
+	// dan menyimpan nilai awal pada variable NilaiAwal
 	fmt.Print("Masukkan nilai awal: ")
 	fmt.Scanln(&NilaiAwal)
 
 	// Input nilai akhir
 	fmt.Print("Masukkan nilai akhir: ")
 	fmt.Scanln(&NilaiAkhir)
+
+	// pilihan
 	fmt.Println("Ketik 1 untuk mencari nilai ganjil, Ketik 2 Untuk mencari nilai genap")
 	fmt.Print("1 atau 2? :")
 	fmt.Scanln(&Menu)
 
+	// jika user pilih 1 maka dilakukan perhitungan ganjil
 	if Menu == 1 {
 		fmt.Println("Bilangan ganjil antara", NilaiAwal, "dan", NilaiAkhir, "adalah:")
 		for i := NilaiAwal; i <= NilaiAkhir; i++ {
